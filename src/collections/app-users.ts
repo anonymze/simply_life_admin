0008b28c0619faab481e9249707eb3482f80c1ae
anonymze <anodevfr@gmail.com>
2025-03-07 22:20:39 +0100

    add: signature library

--- src/collections/app-users.ts
+++ src/collections/app-users.ts
@@ -12,7 +12,8 @@ export const AppUsers: CollectionConfig = {
 	},
 	auth: {
 		maxLoginAttempts: 4,
-		tokenExpiration: 60 * 60 * 24 * 60, // 60 days
+		// tokenExpiration: 60 * 60 * 24 * 60, // 60 days
+		tokenExpiration: 60 * 5, // 5 minutes
 	},
 	admin: {
 		useAsTitle: "email",
