be19ec6df56a5595b4f80e58a205852fbaddab56
anonymze <anodevfr@gmail.com>
2025-03-07 20:17:01 +0100

    add: signature library

--- src/collections/sponsors.tsx
+++ src/collections/sponsors.tsx
@@ -6,7 +6,7 @@ import { canAccessApi } from "../utils/helper";
 export const Sponsors: CollectionConfig = {
 	access: {
 		read: ({ req }) => canAccessApi(req, ["coach", "staff", "player", "visitor"]),
-		create: ({ req }) => false,
+		create: ({ req }) => canAccessApi(req, []),
 		update: ({ req }) => canAccessApi(req, []),
 		delete: ({ req }) => canAccessApi(req, []),
 	},
