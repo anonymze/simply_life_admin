f820b56cebf4b7897e9d8db965378dda2ff2dbe5
anonymze <anodevfr@gmail.com>
2025-03-26 17:39:48 +0100

    add: signature library

--- src/utils/response/header.ts
+++ src/utils/response/header.ts
@@ -7,10 +7,29 @@ export function createResponseHeader(getCookieApiToken: boolean) {
 	return headers;
 }
 
+/**
+ * @description Check if the token is valid with Payload CMS
+ */
 export async function isValidToken(cookies: RequestCookies) {
 	const token = cookies.get("payload-token");
-
 	if (!token) return false;
+
+	try {
+		const req = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/verify/${token.value}`, {
+			method: "POST", 
+			credentials: "include",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+		const data = await req.json();
+
+		console.log('ici')
+		console.log(data)
+		return true;
+	} catch (err) {
+		return false;
+	}
 
 	return true;
 }
