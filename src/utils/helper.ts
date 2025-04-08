32d91276d21e8f3f520817f3736af082a3c428b6
anonymze <anodevfr@gmail.com>
2025-04-08 02:49:20 +0200

    fix: roles

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -8,13 +8,12 @@ const ORIGIN_APP = "simply-life-app://app";
 type originType = "mobile" | "app";
 
 export const canAccessApi = (req: PayloadRequest, roles: AppUser["role"][], originType?: originType): boolean => {
-	// if (!req.user) return false;
-	// if (req.user.collection === "admins") return true;
-	// // console.log(req.headers.get("cookie"));
-	// if (originType && !verifyOrigin(req.headers?.get("origin"), originType)) return false;
+	if (!req.user) return false;
+	if (req.user.collection === "admins") return true;
+	// console.log(req.headers.get("cookie"));
+	if (originType && !verifyOrigin(req.headers?.get("origin"), originType)) return false;
 
-	// return roles.includes(req.user.role);
-	return true;
+	return roles.includes(req.user.role);
 };
 
 const verifyOrigin = (origin: string | null, originType: originType): boolean => {
