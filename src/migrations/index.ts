7bb74780f48b5c1fdb6a56d2311e8bf246e96a5a
anonymze <anodevfr@gmail.com>
2025-05-15 13:09:30 +0200

    update: versions npm

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,6 @@
-import * as migration_20250508_003538 from './20250508_003538';
 import * as migration_20250512_131603 from './20250512_131603';
-import * as migration_20250512_134359 from './20250512_134359';
+import * as migration_20250508_003538 from './20250508_003538';
+
 
 export const migrations = [
   {
@@ -12,10 +12,5 @@ export const migrations = [
     up: migration_20250512_131603.up,
     down: migration_20250512_131603.down,
     name: '20250512_131603',
-  },
-  {
-    up: migration_20250512_134359.up,
-    down: migration_20250512_134359.down,
-    name: '20250512_134359'
   },
 ];
