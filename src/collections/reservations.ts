cc0e57210ffb5d0f474a83b771c74197ecb5243b
anonymze <anodevfr@gmail.com>
2025-06-16 22:29:20 +0200

    add: newsletter

--- src/collections/reservations.ts
+++ src/collections/reservations.ts
@@ -45,9 +45,6 @@ export const Reservations: CollectionConfig = {
 					});
 				}
 
-				console.log(data.day_reservation);
-				console.log(data.desk);
-
 				const existingReservations = await req.payload.find({
 					collection: "reservations",
 					where: {
@@ -71,8 +68,6 @@ export const Reservations: CollectionConfig = {
 						],
 					},
 				});
-
-				console.log(existingReservations);
 
 				if (existingReservations.docs.length === 0) return;
 
