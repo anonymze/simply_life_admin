eb8c2a053a3aaa5aa5d0a816ffdb11c70be5fae9
anonymze <anodevfr@gmail.com>
2025-06-25 13:57:43 +0200

    add: imports

--- src/collections/messages.ts
+++ src/collections/messages.ts
@@ -88,7 +88,7 @@ export const Messages: CollectionConfig = {
 						}
 
 						const requests = chatRoom.guests.map((guest) => {
-							if (typeof guest === "string" || !guest.notifications_token || guest.id === data?.app_user) return;
+							if (typeof guest === "string" || !guest.notifications_token || chatRoom.id === data?.app_user) return;
 							console.log(guest.id)
 							console.log(data?.app_user)
 							return fetch("https://api.expo.dev/v2/push/send", {
