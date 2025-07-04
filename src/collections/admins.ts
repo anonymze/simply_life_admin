088feb7e81c5e39c63fc1cb46cc498972f59bc93
anonymze <anodevfr@gmail.com>
2025-07-04 17:14:04 +0200

    fix: origin

--- src/collections/admins.ts
+++ src/collections/admins.ts
@@ -1,83 +1,84 @@
 import { canAccessApi, validatePassword } from "@/utils/helper";
 import { CollectionConfig } from "payload";
 
-
 export const Admins: CollectionConfig = {
-	slug: "admins",
-	access: {
-		read: ({ req }) => canAccessApi(req, []),
-		create: ({ req }) => canAccessApi(req, []),
-		update: ({ req }) => canAccessApi(req, []),
-		delete: ({ req }) => canAccessApi(req, []),
-	},
-	labels: {
-		singular: {
-			en: "Admin",
-			fr: "Administrateur",
-		},
-		plural: {
-			en: "Admins",
-			fr: "Administrateurs",
-		},
-	},
-	admin: {		
-		group: {
-			en: "Users",
-			fr: "Utilisateurs",	
-		},
-		// defaultColumns: ["name", "range", "price", "priceType", "threshold"],
-		useAsTitle: "email",
-	},
-	// email and password added by default
-	
-	auth: {
-		useAPIKey: true,		
-		maxLoginAttempts: 4,
-		tokenExpiration: 60 * 60 * 24 * 30, // 30 days
-	},
-	hooks: {
-		beforeValidate: [validatePassword],
-	}, 	
-	fields: [
-		{
-			name: "fullname",
-			type: "text",
-			required: true,
-			label: {
-				en: "Full name",
-				fr: "Nom complet",
-			},
-		},
-		{
-			name: "description-below-password",
-			type: "ui",
-			admin: {
-				components: {
-					Field: "/components/description-below-password.tsx",
-				},
-			},
-		},
-		// {
-		// 	name: "actions",
-		// 	type: "ui",
-		// 	admin: {
-		// 		position: "sidebar",
-		// 		components: {
-		// 			Field: "/components/custom-client-field.tsx",
-		// 		},
-		// 	},
-		// },
-		// {
-		// 	name: "serverActions",
-		// 	type: "ui",
-		// 	admin: {
-		// 		position: "sidebar",
-		// 		components: {
-		// 			Field: "/components/custom-server-field.tsx",
-		// 		},
-		// 	},
-		// },
-	],
+  slug: "admins",
+  access: {
+    read: ({ req }) => canAccessApi(req, []),
+    create: ({ req }) => canAccessApi(req, []),
+    update: ({ req }) => canAccessApi(req, []),
+    delete: ({ req }) => canAccessApi(req, []),
+  },
+  labels: {
+    singular: {
+      en: "Admin",
+      fr: "Administrateur",
+    },
+    plural: {
+      en: "Admins",
+      fr: "Administrateurs",
+    },
+  },
+  admin: {
+    group: {
+      en: "Users",
+      fr: "Utilisateurs",
+    },
+    // defaultColumns: ["name", "range", "price", "priceType", "threshold"],
+    useAsTitle: "email",
+  },
+  // email and password added by default
+
+  auth: {
+    useAPIKey: true,
+    maxLoginAttempts: 4,
+    tokenExpiration: 60 * 60 * 24 * 30, // 30 days
+    cookies: {
+      secure: true,
+      sameSite: "None",
+    },
+  },
+  hooks: {
+    beforeValidate: [validatePassword],
+  },
+  fields: [
+    {
+      name: "fullname",
+      type: "text",
+      required: true,
+      label: {
+        en: "Full name",
+        fr: "Nom complet",
+      },
+    },
+    {
+      name: "description-below-password",
+      type: "ui",
+      admin: {
+        components: {
+          Field: "/components/description-below-password.tsx",
+        },
+      },
+    },
+    // {
+    // 	name: "actions",
+    // 	type: "ui",
+    // 	admin: {
+    // 		position: "sidebar",
+    // 		components: {
+    // 			Field: "/components/custom-client-field.tsx",
+    // 		},
+    // 	},
+    // },
+    // {
+    // 	name: "serverActions",
+    // 	type: "ui",
+    // 	admin: {
+    // 		position: "sidebar",
+    // 		components: {
+    // 			Field: "/components/custom-server-field.tsx",
+    // 		},
+    // 	},
+    // },
+  ],
 };
-
-
